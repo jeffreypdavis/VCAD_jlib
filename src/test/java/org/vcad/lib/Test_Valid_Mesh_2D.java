43d4fe5550192b76435186b329329a79a7b61a5b
Jeffrey Davis <jeffreypdavis@yahoo.com>
2017-12-23 14:10:22 -0500

    Added license

--- src/test/java/org/vcad/lib/Test_Valid_Mesh_2D.java
+++ src/test/java/org/vcad/lib/Test_Valid_Mesh_2D.java
@@ -1,5 +1,19 @@
 /*
- * Test_Valid_Mesh_2D.java
+ * Copyright 2017 Jeffrey Davis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * File: Test_Valid_Mesh_2D.java
  */
 package org.vcad.lib;
 
@@ -7,10 +21,10 @@ import java.util.Iterator;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.Assert;
 
 /**
  *
@@ -53,16 +67,16 @@ public class Test_Valid_Mesh_2D extends TestBase {
         mesh.add(new Facet_2D(new Point_2D(0,0), new Point_2D(5,-5), new Point_2D(10,0)));
 
         Valid_Mesh_2D valid = new Valid_Mesh_2D(mesh);
-        assert(!valid.validate());
-        assert(!valid.pts_on_side_empty());
-        assert(valid.pts_on_side_size() == 1);
+        Assert.assertTrue(!valid.validate());
+        Assert.assertTrue(!valid.pts_on_side_empty());
+        Assert.assertTrue(valid.pts_on_side_size() == 1);
         Iterator<Point_2D> it = valid.pts_on_side_iterator();
-        assert(it.hasNext());
-        assert(Point_2D.is_equal(it.next(), new Point_2D(3,3), precision));
-        assert(valid.too_many_share_side_empty());
-        assert(valid.too_many_share_side_size() == 0);
-        assert(valid.facets_inside_facet_empty());
-        assert(valid.facets_inside_facet_size() == 0);
+        Assert.assertTrue(it.hasNext());
+        Assert.assertTrue(Point_2D.is_equal(it.next(), new Point_2D(3,3), precision));
+        Assert.assertTrue(valid.too_many_share_side_empty());
+        Assert.assertTrue(valid.too_many_share_side_size() == 0);
+        Assert.assertTrue(valid.facets_inside_facet_empty());
+        Assert.assertTrue(valid.facets_inside_facet_size() == 0);
     }
 
     @Test
@@ -77,13 +91,13 @@ public class Test_Valid_Mesh_2D extends TestBase {
         mesh.add(new Facet_2D(new Point_2D(0,10), new Point_2D(5,5), new Point_2D(10,10)));
 
         Valid_Mesh_2D valid = new Valid_Mesh_2D(mesh);
-        assert(!valid.validate());
-        assert(!valid.pts_on_side_empty());
-        assert(valid.pts_on_side_size() == 1);
+        Assert.assertTrue(!valid.validate());
+        Assert.assertTrue(!valid.pts_on_side_empty());
+        Assert.assertTrue(valid.pts_on_side_size() == 1);
         Iterator<Point_2D> it = valid.pts_on_side_iterator();
-        assert(it.hasNext());
-        assert(Point_2D.is_equal(it.next(), new Point_2D(3,3), precision));
-        assert(!valid.too_many_share_side_empty());
+        Assert.assertTrue(it.hasNext());
+        Assert.assertTrue(Point_2D.is_equal(it.next(), new Point_2D(3,3), precision));
+        Assert.assertTrue(!valid.too_many_share_side_empty());
 //        cout << "\ntoo many share side size: " << valid.too_many_share_side_size() << "\n";
 //        for (Valid_Mesh_2D::too_many_share_side_iterator iter = valid.too_many_share_side_begin(); iter != valid.too_many_share_side_end(); ++iter)
 //        {
@@ -95,15 +109,15 @@ public class Test_Valid_Mesh_2D extends TestBase {
 //                        iter2->get_point3()->get_x() << " y: " << iter2->get_point3()->get_y() << "\n";
 //            }
 //        }
-        assert(valid.too_many_share_side_size() == 1);
+        Assert.assertTrue(valid.too_many_share_side_size() == 1);
         Iterator<List<Facet_2D>> it2 = valid.too_many_share_side_iterator();
-        assert(it2.hasNext());
+        Assert.assertTrue(it2.hasNext());
         List<Facet_2D> list = it2.next();
-        assert(list.size() == 3);
-        assert(Facet_2D.is_equal(list.get(0), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(5,5)), precision));
-        assert(Facet_2D.is_equal(list.get(1), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(3,3)), precision));
-        assert(Facet_2D.is_equal(list.get(2), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(2,1)), precision));
-        assert(!valid.facets_inside_facet_empty());
+        Assert.assertTrue(list.size() == 3);
+        Assert.assertTrue(Facet_2D.is_equal(list.get(0), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(5,5)), precision));
+        Assert.assertTrue(Facet_2D.is_equal(list.get(1), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(3,3)), precision));
+        Assert.assertTrue(Facet_2D.is_equal(list.get(2), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(2,1)), precision));
+        Assert.assertTrue(!valid.facets_inside_facet_empty());
 //        System.out.println("\nfacets_inside_facets size: " + valid.facets_inside_facet_size());
 //        for (Iterator<Valid_Mesh_2D.FacetsInsideFacet> iter = valid.facets_inside_facet_iterator(); iter.hasNext();) {
 //            Valid_Mesh_2D.FacetsInsideFacet fif = iter.next();
@@ -122,24 +136,24 @@ public class Test_Valid_Mesh_2D extends TestBase {
 //                        " y: " + f.get_point3().get_y());
 //            }
 //        }
-        assert(valid.facets_inside_facet_size() == 2);
+        Assert.assertTrue(valid.facets_inside_facet_size() == 2);
         Iterator<Valid_Mesh_2D.FacetsInsideFacet> it3 = valid.facets_inside_facet_iterator();
-        assert(it3.hasNext());
+        Assert.assertTrue(it3.hasNext());
         Valid_Mesh_2D.FacetsInsideFacet fif = it3.next();
-        assert(Facet_2D.is_equal(fif.getFacet(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(5,5)), precision));
-        assert(fif.insideFacetsSize() == 2);
+        Assert.assertTrue(Facet_2D.is_equal(fif.getFacet(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(5,5)), precision));
+        Assert.assertTrue(fif.insideFacetsSize() == 2);
         Iterator<Facet_2D> it4 = fif.getInsideFacets();
-        assert(it4.hasNext());
-        assert(Facet_2D.is_equal(it4.next(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(3,3)), precision));
-        assert(it4.hasNext());
-        assert(Facet_2D.is_equal(it4.next(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(2,1)), precision));
-        assert(it3.hasNext());
+        Assert.assertTrue(it4.hasNext());
+        Assert.assertTrue(Facet_2D.is_equal(it4.next(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(3,3)), precision));
+        Assert.assertTrue(it4.hasNext());
+        Assert.assertTrue(Facet_2D.is_equal(it4.next(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(2,1)), precision));
+        Assert.assertTrue(it3.hasNext());
         fif = it3.next();
-        assert(Facet_2D.is_equal(fif.getFacet(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(3,3)), precision));
-        assert(fif.insideFacetsSize() == 1);
+        Assert.assertTrue(Facet_2D.is_equal(fif.getFacet(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(3,3)), precision));
+        Assert.assertTrue(fif.insideFacetsSize() == 1);
         it4 = fif.getInsideFacets();
-        assert(it4.hasNext());
-        assert(Facet_2D.is_equal(it4.next(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(2,1)), precision));
+        Assert.assertTrue(it4.hasNext());
+        Assert.assertTrue(Facet_2D.is_equal(it4.next(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(2,1)), precision));
     }
 
     @Test
@@ -152,11 +166,11 @@ public class Test_Valid_Mesh_2D extends TestBase {
         mesh.add(new Facet_2D(new Point_2D(0,10), new Point_2D(0,0), new Point_2D(5,5)));
 
         Valid_Mesh_2D valid = new Valid_Mesh_2D(mesh);
-        assert(!valid.validate());
-        assert(!valid.pts_on_side_empty());
-        assert(valid.pts_on_side_size() == 1);
+        Assert.assertTrue(!valid.validate());
+        Assert.assertTrue(!valid.pts_on_side_empty());
+        Assert.assertTrue(valid.pts_on_side_size() == 1);
         Iterator<Point_2D> it = valid.pts_on_side_iterator();
-        assert(Point_2D.is_equal(it.next(), new Point_2D(5,5), precision));
+        Assert.assertTrue(Point_2D.is_equal(it.next(), new Point_2D(5,5), precision));
 //        cout << "\ntoo many share side size: " << valid.too_many_share_side_size() << "\n";
 //        for (Valid_Mesh_2D::too_many_share_side_iterator iter = valid.too_many_share_side_begin(); iter != valid.too_many_share_side_end(); ++iter)
 //        {
@@ -168,19 +182,19 @@ public class Test_Valid_Mesh_2D extends TestBase {
 //                        iter2->get_point3()->get_x() << " y: " << iter2->get_point3()->get_y() << "\n";
 //            }
 //        }
-        assert(!valid.too_many_share_side_empty());
-        assert(valid.too_many_share_side_size() == 1);
+        Assert.assertTrue(!valid.too_many_share_side_empty());
+        Assert.assertTrue(valid.too_many_share_side_size() == 1);
         Iterator<List<Facet_2D>> it2 = valid.too_many_share_side_iterator();
-        assert(it2.hasNext());
+        Assert.assertTrue(it2.hasNext());
         List<Facet_2D> list = it2.next();
-        assert(list.size() == 3);
+        Assert.assertTrue(list.size() == 3);
         Iterator<Facet_2D> iter = list.iterator();
-        assert(iter.hasNext());
-        assert(Facet_2D.is_equal(iter.next(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(5,5)), precision));
-        assert(iter.hasNext());
-        assert(Facet_2D.is_equal(iter.next(), new Facet_2D(new Point_2D(0,0), new Point_2D(5.00001,4.99999), new Point_2D(5,5)), precision));
-        assert(iter.hasNext());
-        assert(Facet_2D.is_equal(iter.next(), new Facet_2D(new Point_2D(0,10), new Point_2D(0,0), new Point_2D(5,5)), precision));
+        Assert.assertTrue(iter.hasNext());
+        Assert.assertTrue(Facet_2D.is_equal(iter.next(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(5,5)), precision));
+        Assert.assertTrue(iter.hasNext());
+        Assert.assertTrue(Facet_2D.is_equal(iter.next(), new Facet_2D(new Point_2D(0,0), new Point_2D(5.00001,4.99999), new Point_2D(5,5)), precision));
+        Assert.assertTrue(iter.hasNext());
+        Assert.assertTrue(Facet_2D.is_equal(iter.next(), new Facet_2D(new Point_2D(0,10), new Point_2D(0,0), new Point_2D(5,5)), precision));
 //        System.out.println("\nfacets_inside_facets size: " + valid.facets_inside_facet_size());
 //        for (Iterator<Valid_Mesh_2D.FacetsInsideFacet> iter3 = valid.facets_inside_facet_iterator(); iter3.hasNext();) {
 //            Valid_Mesh_2D.FacetsInsideFacet fif = iter3.next();
@@ -199,16 +213,16 @@ public class Test_Valid_Mesh_2D extends TestBase {
 //                        " y: " + f.get_point3().get_y());
 //            }
 //        }
-        assert(!valid.facets_inside_facet_empty());
-        assert(valid.facets_inside_facet_size() == 1);
+        Assert.assertTrue(!valid.facets_inside_facet_empty());
+        Assert.assertTrue(valid.facets_inside_facet_size() == 1);
         Iterator<Valid_Mesh_2D.FacetsInsideFacet> it3 = valid.facets_inside_facet_iterator();
-        assert(it3.hasNext());
+        Assert.assertTrue(it3.hasNext());
         Valid_Mesh_2D.FacetsInsideFacet fif = it3.next();
-        assert(Facet_2D.is_equal(fif.getFacet(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(5,5)), precision));
-        assert(fif.insideFacetsSize() == 1);
+        Assert.assertTrue(Facet_2D.is_equal(fif.getFacet(), new Facet_2D(new Point_2D(0,0), new Point_2D(10,0), new Point_2D(5,5)), precision));
+        Assert.assertTrue(fif.insideFacetsSize() == 1);
         Iterator<Facet_2D> it4 = fif.getInsideFacets();
-        assert(it4.hasNext());
-        assert(Facet_2D.is_equal(it4.next(), new Facet_2D(new Point_2D(0,0), new Point_2D(5.00001,4.99999), new Point_2D(5,5)), precision));
+        Assert.assertTrue(it4.hasNext());
+        Assert.assertTrue(Facet_2D.is_equal(it4.next(), new Facet_2D(new Point_2D(0,0), new Point_2D(5.00001,4.99999), new Point_2D(5,5)), precision));
     }
 
     @Test
@@ -219,15 +233,15 @@ public class Test_Valid_Mesh_2D extends TestBase {
         Shapes.ellipse(mesh, 10, 20, 4);
 
         Valid_Mesh_2D valid = new Valid_Mesh_2D(mesh);
-        assert(valid.validate());
+        Assert.assertTrue(valid.validate());
 //        for (int i = 0; i < valid.unaligned_points_size(); ++i)
 //        {
 //            Point_2D p(valid.unaligned_point_at(i));
 //            cout << "unaligned_points[" << i << "]: x: " << p.get_x() << " y: " << p.get_y() << "\n";
 //        }
-        assert(valid.pts_on_side_empty());
-        assert(valid.pts_on_side_size() == 0);
-//        assert(within_round(valid.unaligned_point_at(0), Point_2D(5,5), precision));
+        Assert.assertTrue(valid.pts_on_side_empty());
+        Assert.assertTrue(valid.pts_on_side_size() == 0);
+//        Assert.assertTrue(within_round(valid.unaligned_point_at(0), Point_2D(5,5), precision));
 //        for (int i = 0; i < valid.facets_with_facets_size(); ++i)
 //        {
 //            Facet_2D f(valid.facets_with_facets_at(i));
@@ -236,8 +250,8 @@ public class Test_Valid_Mesh_2D extends TestBase {
 //                    " y: " << f.get_point2().get_y() << " p3 x: " << f.get_point3().get_x() <<
 //                    " y: " << f.get_point3().get_y() << "\n";
 //        }
-        assert(valid.facets_inside_facet_empty());
-        assert(valid.facets_inside_facet_size() == 0);
+        Assert.assertTrue(valid.facets_inside_facet_empty());
+        Assert.assertTrue(valid.facets_inside_facet_size() == 0);
 //        for (int i = 0; i < valid.too_many_facets_size(); ++i)
 //        {
 //            Facet_2D f(valid.too_many_facets_at(i));
@@ -246,9 +260,9 @@ public class Test_Valid_Mesh_2D extends TestBase {
 //                    " y: " << f.get_point2().get_y() << " p3 x: " << f.get_point3().get_x() <<
 //                    " y: " << f.get_point3().get_y() << "\n";
 //        }
-        assert(valid.too_many_share_side_empty());
-        assert(valid.too_many_share_side_size() == 0);
-//        assert(is_equal(valid.too_many_facets_at(0), Facet_2D(Point_2D(0,0), Point_2D(10,0), Point_2D(5,5)), precision));
-//        assert(is_equal(valid.too_many_facets_at(1), Facet_2D(Point_2D(0,0), Point_2D(5.00001,4.99999), Point_2D(5,5)), precision));
+        Assert.assertTrue(valid.too_many_share_side_empty());
+        Assert.assertTrue(valid.too_many_share_side_size() == 0);
+//        Assert.assertTrue(is_equal(valid.too_many_facets_at(0), Facet_2D(Point_2D(0,0), Point_2D(10,0), Point_2D(5,5)), precision));
+//        Assert.assertTrue(is_equal(valid.too_many_facets_at(1), Facet_2D(Point_2D(0,0), Point_2D(5.00001,4.99999), Point_2D(5,5)), precision));
     }
 }
